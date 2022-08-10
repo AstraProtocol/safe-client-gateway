7c6ee50f609106028c7c6f24121ab12ac911571e
hoanguyenkh <hoa.nguyen2@tiki.vn>
2022-08-10 16:46:46 +0700

    update time duration

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -38,7 +38,7 @@ pub fn scheme() -> String {
 
 // TIME DURATION VALUES
 fn indefinite_timeout() -> usize {
-    env_with_default("INDEFINITE_TIMEOUT", 5 * 1000)
+    env_with_default("INDEFINITE_TIMEOUT", 8 * 1000)
 }
 
 pub fn short_error_duration() -> usize {
